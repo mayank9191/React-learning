e8b2c6c42f44ed5d30fd188e7bbf3a1a1d71d2dc
Mayank Kulahara <mayankkulahara@gmail.com>
2025-02-20 19:56:37 +0530

    tailwind integration and passing props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card.jsx'
+
+function App() {
+
+  let myObject = {
+    username: "mayank",
+    age: 21
+  }
+
+  let newArr = [1, 2, 3, 4]
+
+  return (
+    <>
+
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      <Card username="mayank" price="1.3 sol" />
+      <Card username="krishna" price="1.8 sol" />
+
+    </>
+  )
+}
+
+export default App
