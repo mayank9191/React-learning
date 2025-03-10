5fc5b1a22c71bbbd44131a5867cb53a8d0579cfa
Mayank Kulahara <mayankkulahara@gmail.com>
2025-03-10 23:14:30 +0530

    few changes

--- 06projectCurrencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06projectCurrencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,32 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 
-function useCurrencyInfo(currency) {
+// function useCurrencyInfo(currency) {
 
-  const [data, setData] = useState({})
-  useEffect(() => {
-    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-      .then((res) => res.json())
-      .then((res) => setData(res[currency]))
-      .catch((error) => error)
-  }, [currency])
+//   const [data, setData] = useState({})
+//   useEffect(() => {
+//     fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+//       .then((res) => res.json())
+//       .then((res) => setData(res[currency]))
+//       .catch((error) => error)
+//   }, [currency])
 
-  return data
+//   return data
+// }
+
+// export default useCurrencyInfo
+
+async function currencyDetails(from, to, fromAmmount) {
+
+  const res = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from}.json`)
+
+  const data = await res.json()
+
+  const price = data[from][to]
+
+  console.log(Math.round(Number(price) * fromAmmount), to)
 }
 
-export default useCurrencyInfo
+
+
+
+currencyDetails('krw', 'inr', 10000000)
